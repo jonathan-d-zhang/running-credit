fb3f6aabe2d34b1db16ec4809238665c7388679a
jonathan-d-zhang <69145546+jonathan-d-zhang@users.noreply.github.com>
2024-01-06 20:07:40 -0500

    chore: Add config for OAuth2 and WebClient

---
+++ app/src/main/java/running_credit/config/WebClientConfig.java
@@ -0,0 +1,21 @@
+package running_credit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+    @Bean
+    public WebClient webClient(OAuth2AuthorizedClientManager authorizedClientManager) {
+        var filter = new ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
+        filter.setDefaultOAuth2AuthorizedClient(true);
+        return WebClient.builder()
+                .apply(filter.oauth2Configuration())
+                .build();
+    }
+
+}
+
